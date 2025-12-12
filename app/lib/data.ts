cad95ffa1ed8445aeb4fe11eb8b383f2d3b16091
kamal126 <kamalchandra265@gmail.com>
2025-12-12 11:51:47 +0530

    remove cd ce

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -10,7 +10,7 @@ import {
 import { formatCurrency } from './utils';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
-cd ne 
+
 export async function fetchRevenue() {
   try {
     // Artificially delay a response for demo purposes.
