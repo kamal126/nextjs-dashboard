4cb349d726d97e8a89b2c5b66b539f3c22501ece
kamal126 <kamalchandra265@gmail.com>
2025-12-19 19:49:58 +0530

    update

--- auth.config.ts
+++ auth.config.ts
@@ -4,17 +4,17 @@ export const authConfig = {
   pages: {
     signIn: '/login',
   },
-  callbacks:{
-    authorized({auth, request: {nextUrl}}){
-        const isLoggedIn = !!auth?.user;
-        const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-        if(isOnDashboard){
-            if(isLoggedIn) return true; // Redirect unauthenticated users to login page
-            return false;
-        }else if(isLoggedIn){
-            return Response.redirect(new URL('/dashboard', nextUrl));
-        }
-        return true;
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+        return false; // Redirect unauthenticated users to login page
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL('/dashboard', nextUrl));
+      }
+      return true;
     },
   },
   providers: [], // Add providers with an empty array for now
