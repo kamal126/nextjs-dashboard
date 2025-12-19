b7a599dc991335d23365e01dba141fb890af8d5e
kamal126 <kamalchandra265@gmail.com>
2025-12-19 19:41:56 +0530

    feat: authenticate function

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,6 +5,9 @@ import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import postgres from "postgres";
 
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
+
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 const FormSchema = z.object({
@@ -136,4 +139,24 @@ export async function deleteInvoice(id: string) {
     `;
 
   revalidatePath("/dashboard/invoices");
+}
+
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if(error instanceof AuthError){
+      switch(error.type){
+        case 'CredentialsSignin':
+          return 'Invalid credientials.';
+          default:
+            return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
 }
